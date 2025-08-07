7c4025bbbd1a8a546652e072c1cfe3e55f51a525
Patricia Otieno <dylanpatricia04@gmail.com>
2025-08-07 14:13:11 +0300

    Add DummyComponent to components folder

---
+++ github-user-search/src/components/Dummycomponents.jsx
@@ -0,0 +1,12 @@
+// src/components/DummyComponent.jsx
+import React from 'react';
+
+const DummyComponent = () => {
+  return (
+    <div>
+      <h2>This is a dummy component</h2>
+    </div>
+  );
+};
+
+export default DummyComponent;
