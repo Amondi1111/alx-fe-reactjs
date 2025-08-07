a2f18d59a0caf3302b00ac593c6fa90b9f437610
Patricia Otieno <dylanpatricia04@gmail.com>
2025-08-07 13:58:19 +0300

    React project for GitHub User Search Application

---
+++ github-user-search/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>GitHub User Search</h1>
+      {/* Search input and results will go here */}
+    </div>
+  );
+}
+
+export default App;
