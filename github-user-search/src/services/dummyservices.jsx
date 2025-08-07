5fa12f764d126fd0a949da8862c6fdc9519eed11
Patricia Otieno <dylanpatricia04@gmail.com>
2025-08-07 14:17:41 +0300

    Add dummyservices to services folder

---
+++ github-user-search/src/services/dummyservices.jsx
@@ -0,0 +1,12 @@
+// src/services/DummyComponent.jsx
+import React from 'react';
+
+const DummyComponent = () => {
+  return (
+    <div>
+      <h2>This is a dummy component</h2>
+    </div>
+  );
+};
+
+export default DummyComponent;
