8c921d9fbb71aaf2d515a80d9d9bf34373c01f58
Patricia Otieno <dylanpatricia04@gmail.com>
2025-08-19 17:36:39 +0300

    Moved Profile.jsx to components folder

---
+++ react-router-advanced/src/components/Profile.jsx
@@ -0,0 +1,21 @@
+import { Routes, Route, Link, Outlet } from "react-router-dom";
+import ProfileDetails from "../pages/ProfileDetails.jsx";
+import ProfileSettings from "../pages/ProfileSettings.jsx";
+
+export default function Profile() {
+  return (
+    <div>
+      <h1>Profile Page</h1>
+      <nav>
+        <Link to="details">Details</Link> | <Link to="settings">Settings</Link>
+      </nav>
+
+      <Routes>
+        <Route path="details" element={<ProfileDetails />} />
+        <Route path="settings" element={<ProfileSettings />} />
+      </Routes>
+
+      
+    </div>
+  );
+}
