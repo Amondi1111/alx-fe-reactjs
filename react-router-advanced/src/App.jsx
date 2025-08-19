afd2397a0fa75c8f3e1063bf19bb38596d602915
Patricia Otieno <dylanpatricia04@gmail.com>
2025-08-19 17:54:02 +0300

    Add browser router to App.jsx

--- react-router-advanced/src/App.jsx
+++ react-router-advanced/src/App.jsx
@@ -1,4 +1,4 @@
-import { Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 
 // Pages
 import Home from "./pages/Home.jsx";
@@ -11,42 +11,37 @@ import ProtectedRoute from "./components/ProtectedRoute.jsx";
 import BlogPost from "./pages/BlogPost.jsx";
 
 function App() {
-  const isAuthenticated = true;
-
   return (
-    <div>
-      <nav style={{ marginBottom: "20px" }}>
-        <Link to="/">Home</Link> |{" "}
-        <Link to="/profile">Profile</Link> |{" "}
-        <Link to="/user/123">User 123</Link> |{" "}
-        <Link to="/blog/1">Blog Post 1</Link>
-      </nav>
+    <BrowserRouter>
+      <div>
+        <nav style={{ marginBottom: "20px" }}>
+          <Link to="/">Home</Link> |{" "}
+          <Link to="/profile">Profile</Link> |{" "}
+          <Link to="/user/123">User 123</Link> |{" "}
+          <Link to="/blog/1">Blog Post 1</Link>
+        </nav>
 
-      <Routes>
-        <Route path="/" element={<Home />} />
+        <Routes>
+          <Route path="/" element={<Home />} />
 
-      
-        <Route
-  path="/profile/*"
-  element={
-    <ProtectedRoute>
-      <Profile />
-    </ProtectedRoute>
-  }
-        >
-          
-          <Route path="details" element={<ProfileDetails />} />
-          <Route path="settings" element={<ProfileSettings />} />
-        </Route>
+          <Route
+            path="/profile/*"
+            element={
+              <ProtectedRoute>
+                <Profile />
+              </ProtectedRoute>
+            }
+          >
+            <Route path="details" element={<ProfileDetails />} />
+            <Route path="settings" element={<ProfileSettings />} />
+          </Route>
 
-      
-        <Route path="/user/:id" element={<User />} />
-        <Route path="/blog/:id" element={<BlogPost />} />
-
-        
-        <Route path="*" element={<NotFound />} />
-      </Routes>
-    </div>
+          <Route path="/user/:id" element={<User />} />
+          <Route path="/blog/:id" element={<BlogPost />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
