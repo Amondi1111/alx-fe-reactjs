771977050d17cd848b8a8d7ea6b9824af37f8334
Patricia Otieno <dylanpatricia04@gmail.com>
2025-08-19 17:43:34 +0300

    Added BlogPost

--- react-router-advanced/src/App.jsx
+++ react-router-advanced/src/App.jsx
@@ -2,30 +2,30 @@ import { Routes, Route, Link } from "react-router-dom";
 
 // Pages
 import Home from "./pages/Home.jsx";
-import Profile from "./pages/Profile.jsx";
+import Profile from "./components/Profile.jsx";
 import ProfileDetails from "./pages/ProfileDetails.jsx";
 import ProfileSettings from "./pages/ProfileSettings.jsx";
 import User from "./pages/User.jsx";
 import NotFound from "./pages/NotFound.jsx";
 import ProtectedRoute from "./components/ProtectedRoute.jsx";
+import BlogPost from "./pages/BlogPost.jsx";
 
 function App() {
-  
-  const isAuthenticated = true; 
+  const isAuthenticated = true;
 
   return (
     <div>
       <nav style={{ marginBottom: "20px" }}>
         <Link to="/">Home</Link> |{" "}
         <Link to="/profile">Profile</Link> |{" "}
-        <Link to="/user/123">User 123</Link>
+        <Link to="/user/123">User 123</Link> |{" "}
+        <Link to="/blog/1">Blog Post 1</Link>
       </nav>
 
       <Routes>
-        
         <Route path="/" element={<Home />} />
 
-        
+      
         <Route
           path="/profile/*"
           element={
@@ -39,8 +39,9 @@ function App() {
           <Route path="settings" element={<ProfileSettings />} />
         </Route>
 
-        
+      
         <Route path="/user/:id" element={<User />} />
+        <Route path="/blog/:id" element={<BlogPost />} />
 
         
         <Route path="*" element={<NotFound />} />
